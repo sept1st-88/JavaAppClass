9bd0f82bea78c0a5eeccb13dfe82702e4443a0e8
TJ <TJ@DESKTOP-J2LIGQ3>
2020-02-18 18:49:14 +0900

    objects update

--- Objects6/src/Card.java
+++ Objects6/src/Card.java
@@ -9,7 +9,7 @@ public class Card {
 //	속성
 	String kind = "";
 	int num = 0;
-	static int width = 100;
+	static int width = 100;	//static - 공유된 값으로 바뀌면은 다 바뀐다 (shared variable)
 	static int height = 250;
 
 }
--- Objects6/src/CardMain.java
+++ Objects6/src/CardMain.java
@@ -10,7 +10,7 @@ public class CardMain {
 		System.out.println("Card.height = " + Card.height);
 		
 		//객체 or 인스턴스
-		Card card1 = new Card();
+		Card card1 = new Card();	//인스턴스는 new를 생성해야지만 불러올 수 있다
 		card1.kind = "Heart";
 		card1.num = 7;
 		
@@ -22,8 +22,8 @@ public class CardMain {
 		System.out.println("card2는 " + card2.kind + ", " + card2.num + "이며, 크기는 " + card2.width + ", " + card2.height);
 		
 		System.out.println("card1의 width와 height를 50, 80으로 변경해본다");
-		card1.width = 50;
-		card1.height = 80;
+		card1.width = 50;		//클래스 변수로 지정되었기 때문에 모든 인스턴스가 공통된다.
+		card1.height = 80;		//클래스 변수는 위에 new를 생성하지 않아도 불러올 수 있다.
 		
 		System.out.println("card1은 " + card1.kind + ", " + card1.num + "이며, 크기는 " + card1.width + ", " + card1.height);
 		System.out.println("card2는 " + card2.kind + ", " + card2.num + "이며, 크기는 " + card2.width + ", " + card2.height);
