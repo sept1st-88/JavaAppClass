a93f0d69821c4b71bafa3f11cd0b4839c196b9a4
sangahpark <sangahpark@192.168.35.132>
2020-02-25 23:01:47 +0900

    StringExam

---
+++ StringExam/src/str/exam/StringLv2_1.java
@@ -0,0 +1,21 @@
+package str.exam;
+
+public class StringLv2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// 절대 원본 데이터를 변경하지 않는다
+		String originStr = "자바를 자바라"; // 기준
+		String changeStr = "";          // 결과   
+		
+		
+//		문자열 안에서 마지막 문자가 무엇인지 출력하시오 
+		
+		System.out.println(originStr);
+		System.out.println(changeStr);
+		
+		
+	}
+
+}
